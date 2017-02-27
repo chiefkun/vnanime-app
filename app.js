fdc6314e44548d6a65aec4146943492feac6f6ce
thanhnguyenbk <51303674@hcmut.edu.vn>
2017-02-27 19:58:00 +0700

    Update main

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var {app} = require('electron');
-var {BrowserWindow} = require('electron');
-var {Menu} = require('electron');
-var {shell} = require('electron');
+var {app, BrowserWindow, Menu, shell} = require('electron');
+var {ipcMain} = require('electron');
 var fs = require('fs');
 var path = require('path')
 
 var mainWindow = null;
 var loadingPage = null;
+var cbox = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -20,6 +19,17 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   createApplicationMenu();
   openWindow();
+
+  ipcMain.on('login-complete', function() {
+    // loadingPage.show();
+    mainWindow.hide();
+    cbox = new BrowserWindow({width: 800, height: 600, show: true, webPreferences: {nodeIntegration: true, webSecurity: false}, icon:  path.join(__dirname, 'resource/icon/dango.png')});
+    cbox.loadURL('file://' + __dirname + '/resource/main.html');
+    // cbox.hide();
+    cbox.maximize();
+    cbox.openDevTools();
+    // mainWindow.hide();
+  });
 });
 
 var openWindow = function() {
@@ -27,16 +37,21 @@ var openWindow = function() {
   loadingPage.setMenu(null);
   loadingPage.loadURL('file://' + __dirname + '/resource/loading.html');
 
-  mainWindow = new BrowserWindow({width: 800, height: 600, show: false, webPreferences: {nodeIntegration: false}, icon:  path.join(__dirname, 'resource/icon/dango.png')});
-  // mainWindow.setMenu(null);
-  mainWindow.loadURL('http://google.com/');
+  mainWindow = new BrowserWindow({width: 800, height: 600, show: false, webPreferences: {nodeIntegration: true, webSecurity: false}, icon:  path.join(__dirname, 'resource/icon/dango.png')});
+  mainWindow.loadURL('file://' + __dirname + '/resource/login.html');
   mainWindow.webContents.on('did-start-loading', function(){
     // mainWindow.webContents.insertCSS(fs.readFileSync('resource/css/loading-indicator.css', 'utf8'));
     // mainWindow.webContents.executeJavaScript("window.$ = window.jQuery = require('jquery');");
   });
   mainWindow.webContents.on('did-finish-load', function(){
-    mainWindow.maximize();
+    // mainWindow.webContents.insertCSS(fs.readFileSync('resource/css/cbox.css', 'utf8'));
+    // mainWindow.webContents.executeJavaScript("document.getElementsByClassName('pn_reg').style.backgroundColor  = 'red';");
+    // mainWindow.webContents.executeJavaScript('console.log(document.getElementsByTagName("iframe")[1].contentDocument)');
+    // mainWindow.webContents.executeJavaScript(fs.readFileSync('resource/js/main.js', 'utf8'));
+    mainWindow.setMenu(null);
+    // mainWindow.maximize();
     mainWindow.show();
+    // mainWindow.openDevTools();
     loadingPage.hide();
   });
 
